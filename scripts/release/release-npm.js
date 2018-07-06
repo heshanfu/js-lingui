68c5069d8693709a52b6667ed37932f5c66b2045
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2018-07-06 17:34:15 +0200

    chore: publish pre-releases after successful build

--- scripts/release/release-npm.js
+++ scripts/release/release-npm.js
@@ -2,27 +2,43 @@ const fs = require("fs-extra")
 const path = require("path")
 const ora = require("ora")
 const { execSync } = require("child_process")
+const { getVersion } = require("./version")
+
+const argv = require("minimist")(process.argv.slice(2), {
+  alias: { "dry-run": "dryRun" }
+})
 
 const PACKAGES_DIR = "build/packages"
 
-function releasePackage(packagePath) {
-  const { version } = fs.readJsonSync(path.join(packagePath, "package.json"))
+async function releasePackage(version, packagePath) {
   const name = packagePath.split("/").reverse()[0]
   const spinner = ora(`Publishing @lingui/${name}@${version}`)
 
-  try {
-    execSync(`npm publish --access public`, {
-      cwd: packagePath
-    })
-  } catch (e) {
-    spinner.fail(`Version ${version} already published!`)
+  const tags = argv.next ? "--tags next" : ""
+  const cmd = `npm publish --access public ${tags}`
+
+  if (!argv.dryRun) {
+    try {
+      execSync(cmd, {
+        cwd: packagePath
+      })
+    } catch (e) {
+      spinner.fail(`Version ${version} already published!`)
+    }
+  } else {
+    console.log(`DRY RUN: ${cmd}`)
   }
 
   spinner.succeed()
 }
 
-fs
-  .readdirSync(PACKAGES_DIR)
-  .map(directory => path.join(PACKAGES_DIR, directory))
-  .filter(directory => fs.lstatSync(directory).isDirectory())
-  .forEach(releasePackage)
+async function main() {
+  const version = await getVersion(argv.next)
+
+  fs.readdirSync(PACKAGES_DIR)
+    .map(directory => path.join(PACKAGES_DIR, directory))
+    .filter(directory => fs.lstatSync(directory).isDirectory())
+    .map(packagePath => releasePackage(version, packagePath))
+}
+
+main()
