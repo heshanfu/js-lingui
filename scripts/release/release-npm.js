a1044a8690f8957a6a874838f100127f38a1d4c0
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2018-07-06 23:42:19 +0200

    chore: fail on error

--- scripts/release/release-npm.js
+++ scripts/release/release-npm.js
@@ -3,6 +3,7 @@ const path = require("path")
 const ora = require("ora")
 const { execSync } = require("child_process")
 const { getVersion } = require("./version")
+const R = require("ramda")
 
 const argv = require("minimist")(process.argv.slice(2), {
   alias: { "dry-run": "dryRun" }
@@ -14,7 +15,7 @@ async function releasePackage(version, packagePath) {
   const name = packagePath.split("/").reverse()[0]
   const spinner = ora(`Publishing @lingui/${name}@${version}`)
 
-  const tags = argv.next ? "--tags next" : ""
+  const tags = argv.next ? "--tag next" : ""
   const cmd = `npm publish --access public ${tags}`
 
   if (!argv.dryRun) {
@@ -24,21 +25,28 @@ async function releasePackage(version, packagePath) {
       })
     } catch (e) {
       spinner.fail(`Version ${version} already published!`)
+      return false
     }
   } else {
     console.log(`DRY RUN: ${cmd}`)
   }
 
   spinner.succeed()
+  return true
 }
 
 async function main() {
   const version = await getVersion(argv.next)
 
-  fs.readdirSync(PACKAGES_DIR)
-    .map(directory => path.join(PACKAGES_DIR, directory))
-    .filter(directory => fs.lstatSync(directory).isDirectory())
-    .map(packagePath => releasePackage(version, packagePath))
+  const results = await Promise.all(
+    fs
+      .readdirSync(PACKAGES_DIR)
+      .map(directory => path.join(PACKAGES_DIR, directory))
+      .filter(directory => fs.lstatSync(directory).isDirectory())
+      .map(packagePath => releasePackage(version, packagePath))
+  )
+
+  if (results.some(R.equals(false))) process.exit(1)
 }
 
 main()
